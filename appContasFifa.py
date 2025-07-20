cb703f86315487310dc482395cc8159c38f8c88e
Lucas-Navarro <lucasr6out@gmail.com>
2025-07-19 21:36:22 -0300

    começo app pai

---
+++ appContasFifa.py
@@ -0,0 +1,13 @@
+import tkinter as tk
+
+appFifa = tk.Tk()
+
+appFifa.geometry('1024x768')
+appFifa.title('Sequência Fifa Contas')
+appFifa.iconbitmap('./assets/LogoCoins.ico')
+appFifa.configure(background='#2e2e2e')
+
+appFifa.button()
+
+
+appFifa.mainloop()
