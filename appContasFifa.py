c0de21a42118e4e14f0c25269df52d0c8a966fe8
Lucas-Navarro <lucasr6out@gmail.com>
2025-07-25 20:38:29 -0300

    testes

--- appContasFifa.py
+++ appContasFifa.py
@@ -2,12 +2,12 @@ import tkinter as tk
 
 appFifa = tk.Tk()
 
-appFifa.geometry('1024x768')
+appFifa.geometry('768x660')
 appFifa.title('Sequência Fifa Contas')
 appFifa.iconbitmap('./assets/LogoCoins.ico')
 appFifa.configure(background='#2e2e2e')
 
-appFifa.button()
-
+botaoSair = tk.Button(appFifa, text='Sair', command=quit)
+botaoSair.pack()
 
 appFifa.mainloop()
